aa731208b617b13e2a5e5f4dff2942264d1814c3
Brayden Phillips <brayden@Braydens-MacBook-Pro.local>
2020-06-24 09:30:32 +1000

    Cleaned up, added validation & docstrings

--- src/Services/Commerce/Coupons.ts
+++ src/Services/Commerce/Coupons.ts
@@ -1,21 +1,20 @@
+/**
+ * Author: Brayden Phillips
+ */
 import {Inject, Service} from '@tsed/di';
+import {BadRequest} from '@tsed/exceptions';
 import {UseConnection} from '@tsed/typeorm';
 import {validate} from 'class-validator';
 import CommerceCoupon from '../../Database/Entities/CouponEntity';
-import CommerceCouponRepository from '../../Database/Repositories/CouponRepository';
 import CommerceOrder from '../../Database/Entities/OrderEntity';
 import CommerceProduct from '../../Database/Entities/ProductEntity';
-import CommerceProductRepository from '../../Database/Repositories/ProductRepository';
+import CommerceCouponRepository from '../../Database/Repositories/CouponRepository';
 
 @Service()
 export default class CommerceCouponService {
 	@Inject()
 	@UseConnection('default')
 	private readonly couponRepository: CommerceCouponRepository;
-
-	@Inject()
-	@UseConnection('default')
-	private readonly productRepository: CommerceProductRepository;
 
 	public validateOrderCoupon(order: Partial<CommerceOrder>) {
 		// do that
@@ -28,23 +27,68 @@ export default class CommerceCouponService {
 		return true;
 	}
 
+	/**
+	 * Create a new Coupon.
+	 * @remarks
+	 * The coupon parameter is validated before being
+	 * saved.
+	 *
+	 * @param coupon The coupon to be created.
+	 * @see {@link CommerceCoupon | Coupons}
+	 */
 	public async createOne(coupon: Partial<CommerceCoupon>) {
+		const isNotUnique = coupon?.code
+			? !!(await this.couponRepository.find({code: coupon.code})).length
+			: false;
+		if (isNotUnique) {
+			throw new BadRequest('A coupon with this code already exists.');
+		}
+		await validate(new CommerceCoupon(coupon), {
+			skipNullProperties: true,
+		}).then((errors) => {
+			if (errors.length) {
+				const err = new BadRequest('Failed to validate coupon.');
+				err.body = errors.map((i) => Object.values(i.constraints));
+				err.body = err.body.flat(2);
+				throw err;
+			}
+		});
 		return this.couponRepository.save(coupon);
 	}
 
+	/**
+	 * Get a single coupon by id.
+	 * @param id Primary Key (id) of the coupon.
+	 */
 	public getOne(id: number) {
 		return this.couponRepository.findOne(id);
 	}
 
+	/**
+	 * Get all coupons.
+	 * @todo Paginate
+	 */
 	public getAll() {
 		return this.couponRepository.find();
 	}
 
-	public async updateOne(coupon: Partial<CommerceCoupon>) {
+	/**
+	 * Update a single coupon.
+	 * @remarks
+	 * The id is required in the 'coupon' parameter to identify
+	 * the correct coupon to update.
+	 *
+	 * @param coupon The update to be made.
+	 */
+	public async updateOne(coupon: Partial<CommerceCoupon> & {id: number}) {
 		await this.couponRepository.update(coupon.id, coupon);
 		return this.couponRepository.findOne({id: coupon.id});
 	}
 
+	/**
+	 * Hard delete a single coupon.
+	 * @param id Primary Key (id) of the coupon.
+	 */
 	public deleteOne(id: number) {
 		return this.couponRepository.delete(id);
 	}
